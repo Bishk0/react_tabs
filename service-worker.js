dab2f264c806415af8fc4f1ef64aed6c1cbf4dca
Bishko <yurik322412@gmail.com>
2021-07-27 16:21:33 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.7a9f6aa67d7f03d311e4f726639983d5.js"
 );
 
 self.addEventListener('message', (event) => {
